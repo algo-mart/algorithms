59e1f04ff4fb8ea100e6fcdcf3ecd1918a175d73
toluwase <toluwasethomas1@gmail.com>
2022-03-02 19:11:48 +0100

    strstr without predefined method

--- strings/strstr.go
+++ strings/strstr.go
@@ -45,6 +45,18 @@ func strStr2(haystack string, needle string) int {
 "issip"
  */
 
+func strStr(haystack string, needle string) int {
+	if needle == "" || haystack == needle {
+		return 0
+	}
+	for i := 0; i <= (len(haystack) - len(needle)); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
 	fmt.Println(strStr("mississippi","issip"))
 }
