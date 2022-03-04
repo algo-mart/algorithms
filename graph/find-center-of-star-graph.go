06b8320a4d764ad894a61f2b3d89e7218f1dfd70
olusolaa <alaoolusolae@gmail.com>
2022-03-05 00:47:28 +0100

    add to array

--- graph/find-center-of-star-graph.go
+++ graph/find-center-of-star-graph.go
@@ -1,13 +1,11 @@
-package main
+package graph
 
 import "fmt"
 
 func main() {
-	fmt.Println(findCenter([][]int{{1,2},{2,3},{4,2}}))
+	fmt.Println(findCenter([][]int{{1, 2}, {2, 3}, {4, 2}}))
 }
 
 func findCenter(edges [][]int) int {
 	return 0
 }
-
-
